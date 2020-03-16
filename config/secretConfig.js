b059b9a4e08498b5c527bcdc0ffc79dbfcf021e4
Абдулла <49492759+SencoPan@users.noreply.github.com>
2020-03-16 17:47:58 +0300

    Delete secretConfig.js

--- config/secretConfig.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    redis: {
-        url: 'redis://redis-11006.c228.us-central1-1.gce.cloud.redislabs.com:11006',
-        password: 'USIGeXzqXEF3RoKfPxAPQiGSZuKT3IOK',
-        post: '11006'
-    }
-};
